ddc88a100be1905dd97cfcb76e2eded925d157c3
TSUGUMAR <star.jts@gmail.com>
2016-09-30 14:23:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,5 +20,6 @@ app.get('/ui/bharathiar1.jpg', function (req, res) {
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
+  console.log('welcome');
   console.log(`IMAD course app listening on port ${port}!`);
 });
