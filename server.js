dab3af4d117429d4811155a77486e77034180d26
TSUGUMAR <star.jts@gmail.com>
2016-09-30 14:02:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/software-developer-success.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'bharathiar.jpg'));
+  res.sendFile(path.join(__dirname, 'ui', 'bharathiar1.jpg'));
 });
 
 
